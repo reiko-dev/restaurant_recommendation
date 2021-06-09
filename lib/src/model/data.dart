97ac8a31a90fe44d70ded9e0cae9afd91adf059b
Vitor Lucas Pires Cordovil <lukassfifa@gmail.com>
2021-06-09 15:17:52 -0300

    Fully writes and reads data from firebase.

--- lib/src/model/data.dart
+++ lib/src/model/data.dart
@@ -49,18 +49,67 @@ List<Restaurant> getRestaurantsFromQuery(QuerySnapshot snapshot) {
 }
 
 Future<Restaurant> getRestaurant(String restaurantId) {
-  // TODO: Complete the "Get data" step.
-  return Future.value(null);
+  return FirebaseFirestore.instance
+      .collection('restaurants')
+      .doc(restaurantId)
+      .get()
+      .then((doc) => Restaurant.fromSnapshot(doc));
 }
 
+//
+//Makes the use of transaction to make the update of different data about reviews an atomic change.
+//It means: Or all the changes are stored or none of then.
+//
 Future<void> addReview({String restaurantId, Review review}) {
-  // TODO: Complete the "Write data in a transaction" step.
-  return Future.value();
+  final restaurant =
+      FirebaseFirestore.instance.collection('restaurants').doc(restaurantId);
+
+  final newReview = restaurant.collection('ratings').doc();
+
+  return FirebaseFirestore.instance.runTransaction((Transaction transaction) {
+    return transaction
+        .get(restaurant)
+        .then((DocumentSnapshot doc) => Restaurant.fromSnapshot(doc))
+        .then((Restaurant fresh) {
+      final newRatings = fresh.numRatings + 1;
+      final newAverage =
+          ((fresh.numRatings * fresh.avgRating) + review.rating) / newRatings;
+
+      transaction.update(restaurant, {
+        'numRatings': newRatings,
+        'avgRating': newAverage,
+      });
+
+      transaction.set(newReview, {
+        'rating': review.rating,
+        'text': review.text,
+        'userName': review.userName,
+        'timestamp': review.timestamp ?? FieldValue.serverTimestamp(),
+        'userId': review.userId,
+      });
+    });
+  });
 }
 
 Stream<QuerySnapshot> loadFilteredRestaurants(Filter filter) {
-  // TODO: Complete the "Sorting and filtering data" step.
-  return Stream<QuerySnapshot>.value(null);
+  Query collection = FirebaseFirestore.instance.collection('restaurants');
+
+  if (filter.category != null) {
+    collection = collection.where('category', isEqualTo: filter.category);
+  }
+
+  if (filter.city != null) {
+    collection = collection.where('city', isEqualTo: filter.city);
+  }
+
+  if (filter.price != null) {
+    collection = collection.where('price', isEqualTo: filter.price);
+  }
+
+  return collection
+      .orderBy(filter.sort ?? 'avgRating', descending: true)
+      .limit(50)
+      .snapshots();
 }
 
 void addRestaurantsBatch(List<Restaurant> restaurants) {
